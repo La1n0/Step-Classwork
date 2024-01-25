2c3712040d196d2019433148b7c44cf8dea00ef0
Graur Emil <105105432+La1n0@users.noreply.github.com>
2024-01-25 18:59:53 +0200

    Update main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String[] args) {
+      System.out.println("Hello world");
+    int x=30;
+    if(x%2 != 0)
+    {
+      System.out.println("impara");
+    }
+   
+    else
+    {
+      System.out.println("para");
+    } 
+    
+
+      }
+    }
+
